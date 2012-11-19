ae0ff1a044303c651d2105a26e4365541ad3f7c8
nakakaz11 <nakakaz11@gmail.com>
2012-11-20 00:04:25 +0900

    init

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
-
-/*
- * GET home page.
- */
-
+/* GET home page. */
+var title = 'SW ShootingApp+ChatApp(node.js+express+socket.io+ejs+coffee)'
+var desc = 'SW App Test 121119'
 exports.index = function(req, res){
   res.render('index',{
-      title: 'SW ShootingApp+ChatApp(node.js+express+socket.io+ejs+coffee)',
-      desc: 'SW App Test 121119' }
+      title:  title,
+      desc:   desc }
 )};
 exports.game = function(req, res){
-  res.render('game');
-};
+  res.render('game',{
+      title:  title,
+      desc:   desc }
+)};
 exports.gameover = function(req, res){
-	res.render('gameover');
-};
+	res.render('gameover',{
+      title:  title,
+      desc:   desc }
+)};
