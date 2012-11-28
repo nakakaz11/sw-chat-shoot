4cdd81d922d202063a20759a495c090775091059
Kaz Nakamura <nakakaz11@gmail.com>
2012-11-28 14:37:03 +0900

    fix1128

--- public/javascripts/sub_client.js
+++ public/javascripts/sub_client.js
@@ -288,7 +288,7 @@ jQuery(function($) {
   });
   _socket.on("player-message", function(data) {
     var name, user, val, _ref;
-    _ref = data.users.date;
+    _ref = data.users;
     for (name in _ref) {
       val = _ref[name];
       console.log("SW-UserLog:" + ":" + val);
